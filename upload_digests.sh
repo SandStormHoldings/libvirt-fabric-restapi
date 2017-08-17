0bdd76f17440a0d1dc88ce830959eccedbc84011
Guy Romm <guy@ezscratch.com>
2017-08-17 17:17:01 +0300

    digests uploader.

--- upload_digests.sh
+++ upload_digests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for H in $(cat conf_repo/htdigest-hosts.txt | awk '{print $1}') ; do 
-    scp conf_repo/digest.pw $H:/etc/apache2/digest.pw
+    scp -F ssh_config  conf_repo/digest.pw $H:/etc/apache2/digest.pw
     done
